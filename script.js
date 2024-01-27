db30da96c50cabe9af53e69ee31deb474a781c59
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-01-27 23:29:13 +0100

    emitting message

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const socket = io("http://127.0.0.1:3000")
+const socket = io("http://127.0.0.1:3000");
 
-const messageContainer = document.getElementById('message-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+const messageContainer = document.getElementById("message-container");
+const messageForm = document.getElementById("send-container");
+const messageInput = document.getElementById("message-input");
 
-socket.on("chat-message", data => {
-    console.log(data)
-})
+socket.on("chat-message", (data) => {
+  console.log(data);
+});
+
+console.log("script working")
+
+messageForm.addEventListener("submit", async (e) => {
+    // console.log("first subit button clicked")
+  e.preventDefault();
+  let message = messageInput.value;
+  socket.emit("send-chat-message", message)
+  message = "yes"
+});
